6b3472818adfb0830a35fba997ffd81ff3f783d4
igBFranco <igor.franco@aluno.unc.br>
2022-06-05 17:13:01 -0300

    Adding Professional Picker and Date Picker

--- lib/times.dart
+++ lib/times.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class Times extends StatefulWidget {
   const Times({Key? key}) : super(key: key);
@@ -9,6 +10,13 @@ class Times extends StatefulWidget {
 }
 
 class _TimesState extends State<Times> {
+  List<bool> isSelected = [
+    true,
+    false,
+  ];
+  DateTime? _myDateTime;
+  String time = 'Selecione uma Data';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +30,13 @@ class _TimesState extends State<Times> {
       body: Column(
         children: [
           Container(
-            height: 80,
+            height: 60,
             //decoration: BoxDecoration(
             //    color: Color(0xFFBCBFC1),
             //    borderRadius: BorderRadius.circular(15)),
             child: ListTile(
               title: Text(
-                "Cabelo + Barba",
+                "Cabelo",
                 style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
@@ -36,31 +44,131 @@ class _TimesState extends State<Times> {
               ),
               trailing: Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    Text("R\$50,00",
-                        style: TextStyle(
-                            color: Color(0xFF0DA6DF),
-                            fontWeight: FontWeight.bold)),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text("50min")
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 2.0),
+                  child: Column(
+                    children: [
+                      Text("R\$30,00",
+                          style: TextStyle(
+                              color: Color(0xFF0DA6DF),
+                              fontWeight: FontWeight.bold)),
+                      SizedBox(
+                        width: 60,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 18),
+                          child: Text("30min"),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
           Expanded(
-            child: ListView.separated(
-                padding: const EdgeInsets.all(15),
-                itemBuilder: (BuildContext context, int index) {
-                  return Container();
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return Divider();
-                },
-                itemCount: 10),
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  Divider(),
+                  SizedBox(
+                    child: Text("Selecione o Profissional:",
+                        style: GoogleFonts.lexend(
+                          textStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Color(0xFF0DA6DF)),
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      child: Center(
+                        child: ToggleButtons(
+                          isSelected: isSelected,
+                          selectedColor: Colors.white,
+                          fillColor: Color(0xFF0DA6DF),
+                          color: Color(0xFF0DA6DF),
+                          borderWidth: 1,
+                          borderColor: Color(0xFF0DA6DF),
+                          selectedBorderColor: Color(0xFF0DA6DF),
+                          borderRadius: BorderRadius.circular(5),
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: Text(
+                                "Felipe",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: Text(
+                                "Alex",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          ],
+                          onPressed: (int newIndex) {
+                            setState(() {
+                              for (int index = 0;
+                                  index < isSelected.length;
+                                  index++) {
+                                if (index == newIndex) {
+                                  isSelected[index] = true;
+                                } else {
+                                  isSelected[index] = false;
+                                }
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 150,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Color(0xFF0DA6DF)),
+                            onPressed: () async {
+                              _myDateTime = await showDatePicker(
+                                  context: context,
+                                  initialDate: _myDateTime ?? DateTime.now(),
+                                  firstDate: DateTime(2022),
+                                  lastDate: DateTime(2025));
+
+                              setState(() {
+                                time = DateFormat('dd/MM/yyyy')
+                                    .format(_myDateTime!);
+                              });
+                            },
+                            child: Text(
+                              "Selecionar Data",
+                              style: TextStyle(fontSize: 16),
+                            )),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            time,
+                            style: GoogleFonts.lexend(
+                              textStyle: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Color(0xFF666666)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                ],
+              ),
+            ),
           ),
         ],
       ),
